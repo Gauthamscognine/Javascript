aed691a848925d5fbad454514297ed2da03c4cb1
Gauthamscognine <gautham.kedia@cognine.com>
2025-11-13 17:42:43 +0530

    call back js updated

--- callbacks.js
+++ callbacks.js
@@ -1,3 +1,4 @@
+//call back is basically a function being passed to another function 
 function caldis(ori , dis ){
      return ori  - (ori/100)*dis;
     // console.log(total);
@@ -8,6 +9,7 @@ function getbill(callback){
 }
 getbill(caldis);
 
+//---------------------------------------------------------------------------------------------
 
 function doSomething(callback) {
   console.log("Doing something...");
@@ -17,3 +19,17 @@ function doSomething(callback) {
 doSomething(function() {
   console.log("Callback executed!");
 });
+
+//-----------------------------------------------------------------------------------------------------
+
+//call backs can be synch and also asynch , when we make use of a specifc async function then a callbakc will be asyn .
+// below is a async callbakc and above are the sync callbacks  
+function checkcallback(){
+    setTimeout(()=>{
+        console.log("this is chintu here in callback ")
+    },5000)
+}
+checkcallback();
+console.log("checking for  call back  ")
+
+//-----------------------------------------------------------------------------------
