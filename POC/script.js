1b014c7228cecbfad1c3e0762aa5ba2daa30b509
Gauthamscognine <gautham.kedia@cognine.com>
2025-11-19 17:30:54 +0530

    changes made in POC

--- POC/script.js
+++ POC/script.js
@@ -8,17 +8,21 @@ async function fetchMovies() {
     const year = document.getElementById("yearFilter").value;
     const resultsDiv = document.getElementById("results");
 
+    if(name===""){
+        resultsDiv.innerHTML = `<p> Please enter a movie name </p>`
+        return
+    }
 
     let url = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${name}`;
 
     if (type) url += `&type=${type}`;
-    if (year) url += `&y=${year}`;
-    resultsDiv.innerHTML = "<p>Loading...</p>";
+    if (year) url += `&y=${year}`;// query parameters 
 
         try {
-            const res = await fetch(url);
+            const res = await fetch(url);// what if post and how do we implement it 
+            console.log(res)
             const data = await res.json();
-
+            console.log(data)
             console.log("URL sent:", url);
             console.log("FULL API RESPONSE:", data);
 
@@ -33,10 +37,14 @@ async function fetchMovies() {
                     <h3>${movie.Title}</h3>
                     <p>${movie.Year}</p>
                     <p>Type: ${movie.Type}</p>
+                    <p> -----------------------------</p>
                 </div>
-            `).join("")
+            `)
+
 
         } catch (error) {
             resultsDiv.innerHTML = "<p>Error fetching data.</p>";
         }
     }
+// try to implement without async and await // display iin the form of cards as in jio hotstar 
+//npm wh questions // camelcase or pascal case m when and where . 
